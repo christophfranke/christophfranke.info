323e794b76969ffd754c393c8a32cbe7724543af
Christoph Franke <krito@krito.de>
2019-03-09 06:06:02 +0100

    completed projects

--- pages/project.js
+++ pages/project.js
@@ -1,19 +1,28 @@
 import QueryProject from '../query/project'
 import Layout from '../components/layout'
-import { asText } from '../util/prismic'
+import { asText, url } from '../util/prismic'
 import RichText from '../components/richText'
+
 
 const Project = props => {
 	console.log(props)
+	const links = [
+		props.data.project_url.url ? <a href={url(props.data.project_url)} target="_blank" key="project_url">View Project</a> : null,
+		props.data.repository.url ? <a href={url(props.data.repository)} target="_blank" key="repository">View Repository</a> : null,
+	].filter(x => !!x)
 	return (
 		<Layout>
 			<h1>{asText(props.data.title)}</h1>
 			<RichText content={props.data.description} />
+			<p><b>{asText(props.data.role)}</b></p>
+			<p><em>{props.data.techstack.map(item => asText(item.item)).join(', ')}</em></p>
 			<ul>
-			{props.data.features.map((feature, i) => 
-				<li key={i}>{asText(feature.feature)}</li>)
-			}
+				{props.data.features.map((feature, i) =>
+					<li key={i}>{asText(feature.feature)}</li>
+				)}
 			</ul>
+			{links}
+
 		</Layout>
 	)
 }
--- util/prismic.js
+++ util/prismic.js
@@ -1,5 +1,5 @@
 import PrismicDOM from 'prismic-dom'
 
-
 export const asText = x => PrismicDOM.RichText.asText(x)
 export const asHtml = x => PrismicDOM.RichText.asHtml(x)
+export const url = x => PrismicDOM.Link.url(x)
