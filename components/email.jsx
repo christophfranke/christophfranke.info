c5378e4a64162060ee3fef78600d472fd6e80ee4
Christoph Franke <krito@krito.de>
2019-03-10 18:15:05 +0100

    add colors, cloak email

--- components/email.jsx
+++ components/email.jsx
@@ -1,20 +1,65 @@
+import React from 'react'
 import { css, StyleSheet } from 'aphrodite'
 
-const address = 'hallo@christophfranke.info'
+import style from '../style'
 
 const styles = StyleSheet.create({
 	email: {
 		color: 'white',
 		display: 'block',
-		fontSize: '24px'
+		fontSize: '24px',
+		':hover': {
+			color: style.color.red
+		}
 	}
 })
 
-const Email = props => {
-	const { className, ...rest } = props
-	return (
-		<a className={[css(styles.email), className].join(' ')} href={`mailto:${address}`} {...rest}>{address}</a>
-	)
+const rotate = str => {
+	const codeA = 'a'.charCodeAt(0)
+	const codeZ = 'z'.charCodeAt(0)
+
+	return str.split('').map(letter => {
+		const code = letter.charCodeAt()
+		if (code >= codeA && code <= codeZ) {
+			return String.fromCharCode(code - 3)
+		} else {
+			return letter
+		}
+	}).join('')
+}
+
+const prefix = () => {
+	return rotate('pdlowr')
+}
+
+const address = () => {
+	return rotate('kdoor' + '@' + 'fkulvwrskiudqnh' + '.' + 'lqir')
+}
+
+class Email extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			href: '',
+			address: '...'
+		}
+	}
+
+	componentDidMount() {
+		if (process.browser) {
+			this.setState({
+				href: `${prefix()}:${address()}`,
+				address: address()
+			})
+		}
+	}
+
+	render() {
+		const { className, ...rest } = this.props
+		return (
+			<a className={[css(styles.email), className].join(' ')} href={this.state.href} {...rest}>{this.state.address}</a>
+		)
+	}
 }
 
 export default Email
--- style/index.js
+++ style/index.js
@@ -1,5 +1,7 @@
 export default {
 	color: {
-		blue: 'rgb(57, 89, 142)'
+		blue: 'rgb(57, 89, 142)',
+		green: 'rgb(111, 226, 197)',
+		red: 'rgb(209, 73, 95)'
 	}
 }
