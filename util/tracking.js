88081e84dd0df62fd445b885f1fb88b4b33016d7
Christoph Franke <krito@krito.de>
2020-02-14 13:47:25 +0100

    remove keen from project

--- util/tracking.js
+++ util/tracking.js
@@ -1,34 +1,4 @@
-import KeenTracking from 'keen-tracking'
-
-
-const client = new KeenTracking({
-  projectId: '5cacc49ac9e77c00012203f2',
-  writeKey: process.env.KEEN_WRITE_KEY
-});
-
-const view = url => {
-  const host = process.browser ? window.location.host :
-    (process.env.NODE_ENV === 'development' ? 'localhost' : 'christophfranke.info')
-
-  if (url.length > 1 && url[url.length-1] === '/') {
-    url = url.substr(0, url.length - 1);
-  }
-  
-  client.recordEvent('view', {
-    url,
-    host,
-    ip_address: "${keen.ip}",
-    keen: {
-      addons: [{
-        name: "keen:ip_to_geo",
-        input: {
-          ip: "ip_address"
-        },
-        output: "keen.location"
-      }]
-    }  
-  })
-}
+const view = url => {}
 
 export default {
   view
